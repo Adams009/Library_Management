30238c3481803e44ff5c1bbf4e9aaa6670161fad
Adams009 <olaadam003@gmail.com>
2024-08-27 23:33:09 +0200

    update app.py by setting up datbase config

--- app.py
+++ app.py
@@ -1,6 +1,11 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 
-app = Flask(__name__)
+app = Flask(__name__) # create a Flask instance
+app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///library.db" #setup the database type and location
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False # setup the modification tracker
+
+db = SQLAlchemy(app) # create an SQLAlchemy instance
 
 if __name__ == '__main__':
     app.run()
