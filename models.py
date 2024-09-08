b197ec8291706b0398099c4b40ac958baa1c2ec5
Adams009 <olaadam003@gmail.com>
2024-09-08 07:04:27 +0200

    add book class and some book methods in models

--- models.py
+++ models.py
@@ -21,3 +21,43 @@ class User(db.Model):
         Verify the password provided by the user against the hashed password stored in the database
         """
         return check_password_hash(self.password, password)
+    
+class Book(db.Model):
+    id = db.Column(db.Integer, primary_key=True) # create a unique identifier for each book
+    title = db.Column(db.String(150), nullable=False) # title of the book
+    author = db.Column(db.String(150), nullable=False) # author of the book
+    year = db.Column(db.Integer, nullable=False) # year the book was published
+    isbn = db.Column(db.String(150), nullable=False, unique=True) # unique ISBN for each book
+    total_copies = db.Column(db.Integer, nullable=False)
+    available_copies = db.Column(db.Integer, nullable=False)
+    available = db.Column(db.Boolean, default=True)
+
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False) # create a relationship between the book and the user
+
+    borrowed_books = db.relationship('Borrowed', back_populates='book', lazy=True) # one-to-many relationship with Borrowed model
+
+    def update_availability(self):
+        self.available = self.available_copies > 0
+
+    def serialize(self):
+        return {
+            'id': self.id,
+            'title': self.title,
+            'author': self.author,
+            'year': self.year,
+            'isbn': self.isbn,
+            'available': self.available
+        }
+    
+    # def borrow_book(self, user_id):
+    #     if self.available_copies > 0:
+    #         self.available_copies -= 1
+    #         borrowed = Borrowed(user_id=user_id, book_id=self.id)
+    #         db.session.add(borrowed)
+    #         db.session.commit()
+    #         self.update_availability()
+    #         return True
+    #     return False
+
+
+
