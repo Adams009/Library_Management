c8d4a48a070ab562768683f35b753bb97573e815
Adams009 <olaadam003@gmail.com>
2024-09-09 11:11:28 +0200

    update the comment in db_check folder

--- db_check.py
+++ db_check.py
@@ -1,7 +1,7 @@
 from app import app, db
 from sqlalchemy import inspect
 
-with app.app_context():
-    inspector = inspect(db.engine)
-    tables = inspector.get_table_names()
+with app.app_context(): # needed to use the app context to access the database within the app
+    inspector = inspect(db.engine) # needed to inspect the database
+    tables = inspector.get_table_names() # needed to get the table names
     print(tables)
