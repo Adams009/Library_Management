8bd91c55928a25608a9929ad79a05b290c5b0334
Adams009 <olaadam003@gmail.com>
2024-09-09 11:10:38 +0200

    update the comment in it

--- app.py
+++ app.py
@@ -4,9 +4,9 @@ from flask_migrate import Migrate
 
 app = Flask(__name__) # create a Flask instance
 app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///library.db" #setup the database type and location
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False # setup the modification tracker
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False # setup the modification tracker for the database to False
 
-db.init_app(app) # create an SQLAlchemy instance
+db.init_app(app) # create an SQLAlchemy instance for the database
 migrate = Migrate(app,db) # create a Migration instance
 
 if __name__ == '__main__':
