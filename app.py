3d4becdcefe8af892a795379643b775e3f1814d3
Adams009 <olaadam003@gmail.com>
2024-09-13 20:45:09 +0200

    register the borrow blueprint to the app

--- app.py
+++ app.py
@@ -11,9 +11,11 @@ migrate = Migrate(app,db) # create a Migration instance
 
 from blueprints.user_routes import users_bp
 from blueprints.book_routes import books_bp
+from blueprints.borrow_routes import borrow_bp
 
 app.register_blueprint(users_bp, url_prefix='/api') # register the blueprint for the users
 app.register_blueprint(books_bp, url_prefix='/api') # register the blueprint for the books
+app.register_blueprint(borrow_bp, url_prefix='/api') # register the blueprint for the borrowed books
 
 if __name__ == '__main__':
     app.run(debug=True) # run the app in debug mode
